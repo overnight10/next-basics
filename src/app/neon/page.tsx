f7915853f86c765ec1d9e25ea6906b4b813ab8c2
overnight10 <isaistrujillo1050100@gmail.com>
2024-12-15 16:13:26 -0500

    Printing env in /neon page

--- src/app/neon/page.tsx
+++ src/app/neon/page.tsx
@@ -2,6 +2,8 @@ import { neon } from '@neondatabase/serverless';
 import nextConfig from '../../../next.config';
 
 async function getData() {
+  const url = nextConfig?.env?.DATABASE_URL;
+  if (!url) return 'No database url found';
   const sql = neon(nextConfig?.env?.DATABASE_URL as string);
   const response = await sql`SELECT version()`;
   return response[0].version;
@@ -9,5 +11,19 @@ async function getData() {
 
 export default async function Page() {
   const data = await getData();
-  return <>{data}</>;
+  return <div>
+    <span>Neon version: {data}</span>
+    <br />
+    <br />
+    <h2>Next.js config</h2>
+    <ul>{Object.entries(nextConfig?.env ?? {}).map(([key, value]) => <li key={key}>{key}: {value}</li>)}</ul>
+    <br />
+    <br />
+    <h2>Process env</h2>
+    <ul>
+      {
+        Object.entries(process?.env ?? {}).map(([key, value]) => <li key={key}>{key}: {value}</li>)
+      }
+    </ul>
+  </div>;
 }
